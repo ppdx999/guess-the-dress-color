349300649569002f1e732a8172a10cf99c5e5023
ppdx999 <ppdx999@gmail.com>
2024-01-05 10:27:09 +0900

    close #7 スクリーン表示画面にスリープをOffにするボタンをつける

--- app/routes/main.tsx
+++ app/routes/main.tsx
@@ -1,6 +1,7 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { useWakeLock } from "react-screen-wake-lock";
 
 import { getChoiceByUserId } from "~/models/choice.server";
 import { requireUserId } from "~/session.server";
@@ -19,11 +20,22 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
 export default function MainPage() {
   const { choice } = useLoaderData<typeof loader>();
+  const { request, released } = useWakeLock();
 
   return (
     <main className="relative h-screen">
       <div className="mx-auto h-screen flex justify-center items-center">
         <div className={`absolute inset-0 ${choice.dressColorCode}`} />
+        {released != false ? (
+          <button
+            className="absolute bottom-0 m-4 p-4 bg-gray-200 rounded-full"
+            onClick={() => {
+              request();
+            }}
+          >
+            スリープをOFFにする
+          </button>
+        ) : null}
       </div>
     </main>
   );
